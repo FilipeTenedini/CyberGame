9ac078af618488c9f63e3358797ed9bf289af7c1
Tio Léo <95362445+leo-md@users.noreply.github.com>
2022-11-17 07:32:34 -0300

    Add files via upload

--- admin/functions.php
+++ admin/functions.php
@@ -15,7 +15,6 @@ function login($connect){
         $query = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha' ";
         $executar = mysqli_query($connect, $query);
         $return = mysqli_fetch_assoc($executar);
-
         if (!empty($return['email'])) {
             //echo 'Bem vindo '.$return['nome'];
             session_start();
@@ -27,8 +26,7 @@ function login($connect){
                 header("location: index.php");
             }else{
                 header("location: perfil.php");
-            }
-            
+            }            
         }else{
             echo 'Usuário ou senha não encontrados';
         }        
@@ -43,7 +41,7 @@ function logout(){
 }
 /*Seleciona(busca) no BD apenas um resultado com bse no ID*/
 function buscaUnica($connect, $tabela, $id){
-    $query ="SELECT FROM $tabela WHERE id ="  . (int) $id;
+    $query = "SELECT * FROM $tabela WHERE id ="  . (int) $id;
     $execute = mysqli_query($connect, $query);
     $result = mysqli_fetch_assoc($execute);
     return $result;
@@ -58,7 +56,6 @@ function buscar($connect, $tabela, $where = 1, $order=""){
     $results = mysqli_fetch_all($execute, MYSQLI_ASSOC);
     return $results;
 }
-
 /*Inserir novos usuários*/
 function inserirUsuarios($connect){
 
@@ -78,7 +75,6 @@ function inserirUsuarios($connect){
         if (!empty($verifica)) {
             $erros[] = "E-mail já cadastrado.";
         }
-
         if (empty($erros)) {
             //inserir o usuário no BD
             $query = "INSERT INTO usuarios (nome, email, senha, dataCadastro) VALUES ('$nome' , '$email' , '$senha' , NOW())";
@@ -96,8 +92,6 @@ function inserirUsuarios($connect){
         }    
     }
 }
-
-
 //Deletar algum dado
 function deletar($connect, $tabela, $id){
     if (!empty($id)) {
@@ -109,4 +103,192 @@ function deletar($connect, $tabela, $id){
             echo "Erro ao deletar!";
         }
     }
+}
+
+function updateUser($connect){
+    if (isset($_POST['atualizar']) AND !empty($_POST['email'] ) ) {
+        $erros = array();
+        $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $nome = mysqli_real_escape_string($connect, $_POST['nome']);
+        $senha = '';
+        $data = mysqli_real_escape_string($connect, $_POST['dataCadastro']);
+
+        if (empty($data)) {
+            $erros[] = 'Preencha a data de cadastro.';
+        }
+        if (empty($email)) {
+            $erros[] = 'Preencha seu e-mail corretamente.';
+        }
+        if (strlen($nome) < 4) {
+            $erros[] = 'Preencha o nome completo.';
+        }
+        if (!empty($_POST['senha'])) {
+            if ($_POST['senha'] == $_POST['repetesenha']) {
+                $senha = sha1($_POST['senha']);               
+            }else{
+                $erros[] = "Senhas não conferem";
+            }
+        }
+        $queryEmailAtual = "SELECT email FROM usuarios WHERE id = $id";
+        $buscaEmailAtual = mysqli_query($connect, $queryEmailAtual);
+        $retornoEmail = mysqli_fetch_assoc($buscaEmailAtual);
+                        
+        $queryEmail =  "SELECT email FROM usuarios WHERE email= '$email' AND email <> '" . $retornoEmail['email'] . "'";
+        $buscaEmail =   mysqli_query($connect, $queryEmail);
+        $verifica = mysqli_num_rows($buscaEmail);
+
+        if (!empty($verifica)) {
+            $erros[] = "E-mail já cadastrado.";
+        }
+
+        if (empty($erros)) {
+            //UPDATE usuario
+            if (!empty($senha)) {
+                $query = "UPDATE usuarios SET nome = '$nome', email = '$email', senha = '$senha', dataCadastro = '$data' WHERE id =" . (int) $id;
+            }else{
+                $query = "UPDATE usuarios SET nome = '$nome', email = '$email', dataCadastro = '$data' WHERE id =" . (int) $id;
+            }
+            $executar = mysqli_query($connect, $query);
+            if ($executar) {
+                echo "Usuario atualizado com sucesso!";
+            } else {
+                echo "Erro ao atualizar o usuário.";
+            }
+        }else{
+            foreach ($erros as $erro) {
+                echo "<p>$erro</p>";
+            } 
+        }
+    }    
+}
+
+function insertCardapio($connect){
+    if ((isset($_POST['insert']) AND !empty($_POST['titulo']) AND !empty($_POST['descricao'])) ) {
+
+        $titulo = mysqli_real_escape_string($connect, $_POST['titulo']);
+        $descricao = mysqli_real_escape_string($connect, $_POST['descricao']);
+        $data = mysqli_real_escape_string($connect, $_POST['dataRegistro']);
+        
+        $imagem = !empty($_FILES['imagem']['name']) ?  $_FILES['imagem']['name'] : "";
+        $retornoUpload = "";
+        if (!empty($imagem)) {
+            $caminho = "imagens/uploads/";
+            $retornoUpload = uploadImage($caminho);
+            if (is_array($retornoUpload)) {
+                foreach ($retornoUpload as $erro) {
+                    echo $erro;
+                }
+                $imagem = "";
+            }else{
+                $imagem = $retornoUpload;
+            }
+        }       
+        
+        $query = "INSERT INTO cardapio (titulo, descricao, imagem, dataRegistro) VALUES ('$titulo' , '$descricao' , '$imagem' , '$data')";
+        $executar = mysqli_query($connect, $query);
+        if ($executar) {
+            if (is_array($retornoUpload)){
+                echo "Item inserido com sucesso! Porém a imagem não pode ser inserida.";
+            }else{
+                header("location: cardapio.php");
+            }            
+        } else {
+            echo "Erro ao inserir o usuário.";
+        }       
+    }
+}
+
+function updateCardapio($connect){
+    if ((isset($_POST['update']) AND !empty($_POST['titulo']) AND !empty($_POST['descricao'])) ) {
+        $id = (int) $_POST['id'];
+        $titulo = mysqli_real_escape_string($connect, $_POST['titulo']);
+        $descricao = mysqli_real_escape_string($connect, $_POST['descricao']);
+        $data = mysqli_real_escape_string($connect, $_POST['dataRegistro']);
+        
+        $imagem = !empty($_FILES['imagem']['name']) ?  $_FILES['imagem']['name'] : "";
+        $retornoUpload = "";
+        if (!empty($imagem)) {
+            $caminho = "imagens/uploads/";
+            $retornoUpload = uploadImage($caminho);
+            if (is_array($retornoUpload)) {
+                foreach ($retornoUpload as $erro) {
+                    echo $erro;
+                }
+                $imagem = "";
+            }else{
+                $imagem = $retornoUpload;
+            }
+        } 
+
+        if (!empty($id)) {
+            if (!empty($imagem)) {
+                $query = "UPDATE cardapio SET imagem = '$imagem', titulo = '$titulo', descricao = '$descricao', dataRegistro = '$data' WHERE id = '$id'";
+
+            }else{
+                $query = "UPDATE cardapio SET titulo = '$titulo', descricao = '$descricao', dataRegistro = '$data' WHERE id = '$id'";
+            }
+            $executar = mysqli_query($connect, $query);
+            if ($executar) {
+                if (is_array($retornoUpload)){
+                    echo "Item atualizado com sucesso! Porém a imagem não pode ser inserida.";
+                }else{
+                    header("location: cardapio.php");
+                }
+            } else {
+                echo "Erro ao atualizar o cardapio.";
+            }
+        }
+
+    }
+}
+
+
+function uploadImage($caminho){
+    if (!empty($_FILES['imagem']['name'])) {
+        $nomeImagem = $_FILES['imagem']['name'];
+        $tipo = $_FILES['imagem']['type'];
+        $nomeTemporario = $_FILES['imagem']['tmp_name'];
+        $tamanho = $_FILES['imagem']['size'];
+        $erros = array();
+
+        $tamanhoMaximo = 1024 * 1024 * 5; //+/- 5 mb
+        if ($tamanho > $tamanhoMaximo) {
+            $erros[] = 'Seu arquivo excede o tamanho máximo (5mb).<br>';
+        }
+        
+        $arquivosPermitidos = ["png", "jpg", "jpeg"];
+        $extensao = pathinfo($nomeImagem, PATHINFO_EXTENSION);
+        if (!in_array($extensao, $arquivosPermitidos)) {
+            $erros[] = 'Seu arquivo não possui uma extensão permitida (png, jpg, jpeg).<br>';
+        }
+
+        $typesPermitidos = ["image/png","image/jpg","image/jpeg"];
+        if (!in_array($tipo, $typesPermitidos)) {
+            $erros[] = 'Seu arquivo não possui um tipo permitido (png, jpg, jpeg).<br>';
+
+        }
+
+        if (!empty($erros)) {
+            /*foreach ($erros as $erro) {
+                echo $erro;
+            }*/
+            return $erros;
+        }else{
+            
+            $hoje = date("d-m-Y_h-i-s");
+            $novoNome = $hoje."-".$nomeImagem;
+            if(move_uploaded_file($nomeTemporario, $caminho.$novoNome)){
+                return $novoNome;
+            }else{
+                return FALSE;
+            }
+            
+        }
+    }
+}
+
+function buscaEstilo($connect){
+    $estilo = buscaUnica($connect, 'skins', 1);
+    return $estilo['nome'];   
 }

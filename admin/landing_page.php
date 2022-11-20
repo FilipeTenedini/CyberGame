b1354b40198c36f212cce966eb84d25deb943535
Pietro Del Corona <106328258+PietroDelCorona@users.noreply.github.com>
2022-11-20 20:34:03 -0300

    landing page 1
    criação da página php do landing page e o respectivo css

---
+++ admin/landing_page.php
@@ -0,0 +1,34 @@
+<?php require "functions.php"
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style_landing_page.css">
+    <title>Cybergame - Página Promocional</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <img class="cabecalho-imagem">
+        <nav class="cabecalho-nav">
+            <a class="cabelhaco-menu-item" href="home.php">Home</a>
+            <a class="cabelhaco-menu-item" href="cardapio.php">Cardápio</a> 
+            <a class="cabelhaco-menu-item" href="login.php">Fazer Login</a>
+            <a class="cabelhaco-menu-item" href="#">Contato</a>
+            <a class="cabelhaco-menu-item" href="#">Trabalhe Conosco</a>
+        </nav>
+    </header>
+    <main class="conteudo"></main>
+        <section class="conteudo-principal">
+
+        </section>
+        <section class="conteudo-secundario">
+            
+        </section>
+        
+    <footer class="rodape"></footer>
+    
+</body>
+</html>

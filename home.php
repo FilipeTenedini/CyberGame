fc090e341101ba9a8350b70dd29ff7384a77b511
Tio Léo <95362445+leo-md@users.noreply.github.com>
2022-12-10 17:55:17 -0300

    Add files via upload
    Integração

--- home.php
+++ home.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
+<?php
+require_once 'admin/functions.php';
+$estilo = buscaEstilo($connect);
+
+?>
+
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css""> 
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles_<?php echo $estilo; ?>.css"> 
     <title>CYBERGAMES</title>
 </head>
 <body>
